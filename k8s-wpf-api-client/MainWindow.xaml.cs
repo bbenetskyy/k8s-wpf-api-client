e77dd7d0c4163afcef2425368891100704d95413
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-05-27 21:59:27 +0200

    Finished UI part

--- k8s-wpf-api-client/MainWindow.xaml.cs
+++ k8s-wpf-api-client/MainWindow.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,17 +12,34 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using DevExpress.Xpf.Core;
 
 namespace k8s_wpf_api_client
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : ThemedWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+
+        private void Get_Pod_List_Click(object sender, RoutedEventArgs e)
+        {
+            output.Items.Add("Get Pod List");
+        }
+
+        private void Kill_Click(object sender, RoutedEventArgs e)
+        {
+            output.Items.Add("Kill");
+        }
+
+        private void Check_Click(object sender, RoutedEventArgs e)
+        {
+            output.Items.Add("Check");
         }
     }
 }
