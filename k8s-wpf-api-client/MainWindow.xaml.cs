fa255f9e8715fa2573ded99e806bee5775331bb3
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-05-27 22:15:43 +0200

    some request logic

--- k8s-wpf-api-client/MainWindow.xaml.cs
+++ k8s-wpf-api-client/MainWindow.xaml.cs
@@ -1,18 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.Linq;
-using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 using DevExpress.Xpf.Core;
+using System.Management.Automation.Runspaces;
+using System.Management.Automation;
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace k8s_wpf_api_client
 {
@@ -21,25 +15,68 @@ namespace k8s_wpf_api_client
     /// </summary>
     public partial class MainWindow : ThemedWindow
     {
+        private Runspace _rs;
+
         public MainWindow()
         {
             InitializeComponent();
+            _rs = RunspaceFactory.CreateRunspace();
+            _rs.OpenAsync();
         }
 
 
         private void Get_Pod_List_Click(object sender, RoutedEventArgs e)
         {
-            output.Items.Add("Get Pod List");
+            using (var ps = PowerShell.Create())
+            {
+                ps.AddScript("kubectl get pods");
+                ps.Runspace = _rs;
+                var collection = ps.Invoke();
+                foreach (var psObject in collection)
+                {
+                    output.Items.Add(psObject.BaseObject);
+                }
+            }
         }
 
-        private void Kill_Click(object sender, RoutedEventArgs e)
+        private async void Kill_Click(object sender, RoutedEventArgs e)
         {
-            output.Items.Add("Kill");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:12789/");
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.PostAsync("/shutdown", null);
+            if (response.IsSuccessStatusCode)
+            {
+                output.Items.Add(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
         }
 
-        private void Check_Click(object sender, RoutedEventArgs e)
+        private async void Check_Click(object sender, RoutedEventArgs e)
         {
-            output.Items.Add("Check");
+            HttpClient client = new HttpClient();
+            client.BaseAddress = new Uri("http://localhost:12789/");
+            client.DefaultRequestHeaders.Accept.Add(
+               new MediaTypeWithQualityHeaderValue("application/json"));
+            HttpResponseMessage response = await client.GetAsync("/");
+            if (response.IsSuccessStatusCode)
+            {
+                output.Items.Add(await response.Content.ReadAsStringAsync());
+            }
+            else
+            {
+                MessageBox.Show("Error Code" + response.StatusCode + " : Message - " + response.ReasonPhrase);
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            _rs.CloseAsync();
+            base.OnClosed(e);
         }
     }
 }
