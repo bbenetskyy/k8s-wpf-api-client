7889a685be32295231d25bce6bad0e55e9a39856
Bohdan Benetskyi <benetskyybogdan@gmail.com>
2018-05-29 11:01:27 +0200

    Some refactoring

---
+++ k8s-wpf-api-client/Commands/HttpCommands.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace k8s_wpf_api_client.Commands
+{
+    public static class HttpCommands
+    {
+        public static async Task<string[]> CheckAsync(string ip,
+            string port, int checkCount = 1)
+        {
+            using (var client = GetHttpClient(ip, port))
+            {
+                return await MakeRequestAsync(ip, port, checkCount,
+                    client.GetAsync("/"));
+            }
+        }
+
+        public static async Task<string[]> KillAsync(string ip,
+            string port, int checkCount = 1)
+        {
+            using (var client = GetHttpClient(ip, port))
+            {
+                return await MakeRequestAsync(ip, port, checkCount,
+                    client.PostAsync("/shutdown", null));
+            }
+        }
+
+        private static async Task<string[]> MakeRequestAsync(string ip,
+            string port,
+            int checkCount,
+            Task<HttpResponseMessage> requestTask)
+        {
+            var resList = new List<string>();
+            for (var i = 0; i < checkCount; i++)
+            {
+                var response = await requestTask;
+                if (response.IsSuccessStatusCode)
+                {
+                    resList.Add(await response.Content.ReadAsStringAsync());
+                }
+                else
+                {
+                    resList.Add($"Error Code {response.StatusCode} : Message - {response.ReasonPhrase}");
+                }
+            }
+
+            return resList.ToArray();
+        }
+
+        private static HttpClient GetHttpClient(string ip,
+            string port)
+        {
+            var client = new HttpClient
+            {
+                BaseAddress = new Uri($"http://{ip}:{port}/")
+            };
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            return client;
+        }
+    }
+}
